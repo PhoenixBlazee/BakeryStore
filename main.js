a43b7d9d07e63c4f14ad31465406bc14f023b204
Manjeet Kumar <160043956+PhoenixBlazee@users.noreply.github.com>
2025-07-31 20:22:10 +0530

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-const navlist = document.getElementById('navlist');
-const hamburger = document.getElementById('hamburger');
-
-hamburger.addEventListener('click', () => {
-    navlist.classList.toggle('navlist-active');
-});
